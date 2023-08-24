a18a18b0b8b07bc3529f59541c08ee7f2a0ab2e9
Julie Lander <juhe0001@stud.kea.dk>
2023-08-24 10:24:09 +0200

    ovelse2

---
+++ script.js
@@ -0,0 +1,21 @@
+const titel = document.querySelector("#titel");
+titel.textContent = "Ny Titel";
+titel.innerHTML = "<h1>Ny Titel</h1>";
+console.log(titel);
+
+const undertitel = document.querySelector("#undertitel");
+undertitel.textContent = "Ny underTitel";
+undertitel.innerHTML = "<h2>Ny undertitel</h2>";
+console.log(undertitel);
+
+const article1 = document.querySelector("#article1");
+article1.textContent = "Ny article1";
+console.log(article1);
+
+const article2 = document.querySelector("#article2");
+article2.textContent = "Ny article2";
+console.log(article2);
+
+const footer = document.querySelector("#footer");
+footer.textContent = "Ny Footer";
+console.log(footer);
